92b88ba482ab3e64d343f162f094d4dd3f6bc370
shuri <inoseshuri@icloud.com>
2025-05-14 21:40:29 +0900

    "更新"

--- index.rb
+++ index.rb
@@ -2,13 +2,9 @@ require "./bowling"
 
 index = 0
 
-
-
 game1 = Bowling.new
 game1.name_ask
 game1.test_game(0,10,0)
 game1.score_info(score)
-#game1.score_info()
 
-#game1 = Bowling.new
-#game1.score_info("shuri",980)
+#game1.score_info()
